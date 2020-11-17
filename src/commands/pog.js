86aea174b0c222a2b2ef9c12ba0e414e4b9cd9a1
goldno <goldno@hotmail.com>
2020-11-16 23:58:28 -0500

    add pog.js to commands

---
+++ src/commands/pog.js
@@ -0,0 +1,14 @@
+import Command from "../models/Command.js";
+
+export default class Pog extends Command {
+  constructor() {
+    super({
+      commandName: 'pog',
+      helpMessage: 'get a pog message from the bot'
+    })
+  }
+
+  async execute(msg) {
+    msg.reply("Pog!")
+  }
+}
